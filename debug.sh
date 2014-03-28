3f457e32499fea97e75a574b5389ae01ec0a0cd3
Mikhail Mandrykin <mandrykin@ispras.ru>
2014-03-28 22:43:59 +0400

    Added debugging script

---
+++ debug.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+SCRIPT='
+load_printer /usr/lib/ocaml/nums.cma
+load_printer src/lib/integer.cmo
+'
+# load_printer cil/src/cil_datatype.cmo
+# load_printer src/printer/printer.cmo
+rlwrap -P "$SCRIPT" ocamldebug -I "$DIR/cil/src" -I "$DIR/cil/src/ext" -I "$DIR/cil/src/frontc" -I "$DIR/cil/src/logic" -I "$DIR/src/" -I "$DIR/src/logic" -I "$DIR/src/printer" -I "$DIR/src/lib" -I "$DIR/src/kernel" -I "$DIR/src/type" -I "$DIR/src/project" $@
