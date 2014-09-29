e86689ac4482e0cccf38f3d1cb604f0ca30efffc
Mikhail Mandrykin <mandrykin@ispras.ru>
2014-09-29 19:31:39 +0400

    Fixed debug script

--- debug.sh
+++ debug.sh
@@ -2,10 +2,13 @@
 
 DIR=$(dirname $(readlink -f $0))
 
-SCRIPT='
-load_printer /usr/lib/ocaml/nums.cma
+WHERE=$(ocamlopt -where)
+
+SCRIPT="
+load_printer ${WHERE}/nums.cma
+load_printer ${WHERE/ocaml/zarith}/zarith.cma
 load_printer src/lib/integer.cmo
-'
+"
 # load_printer cil/src/cil_datatype.cmo
 # load_printer src/printer/printer.cmo
 rlwrap -P "$SCRIPT" ocamldebug -I "$DIR/cil/src" -I "$DIR/cil/src/ext" -I "$DIR/cil/src/frontc" -I "$DIR/cil/src/logic" -I "$DIR/src/" -I "$DIR/src/logic" -I "$DIR/src/printer" -I "$DIR/src/lib" -I "$DIR/src/kernel" -I "$DIR/src/type" -I "$DIR/src/project" $@
