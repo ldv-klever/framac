1ab1ad314c203c31c6025a950ce1312c8f168b0b
Mikhail Mandrykin <mandrykin@ispras.ru>
2016-05-30 18:08:59 +0300

    Change in integer constant typing, fixes wrong typing of INT_MIN of gcc_x86_64, to be checked on more examples

--- cil/src/cil.ml
+++ cil/src/cil.ml
@@ -3909,8 +3909,8 @@ let parseIntAux (str:string) =
     else if hasSuffix "U" then
       1, [IUInt; IULong; IULongLong]
     else
-      0, if octalhexbin || true (* !!! This is against the ISO but it
-                              * is what GCC and MSVC do !!! *)
+      0, if octalhexbin (*|| true*) (* !!! This is against the ISO but it
+                                     * is what GCC and MSVC do !!! *)
         then [IInt; IUInt; ILong; IULong; ILongLong; IULongLong]
         else [IInt; ILong; IUInt; ILongLong]
   in
