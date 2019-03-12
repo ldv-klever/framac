8e89134dad9828cda0081783fcf013c3a0c94eed
Mikhail Mandrykin <mandrykin@ispras.ru>
2019-03-12 17:34:55 +0300

    Fixed additional check for lemma functions

--- src/kernel_services/ast_queries/logic_lemma_functions.ml
+++ src/kernel_services/ast_queries/logic_lemma_functions.ml
@@ -173,7 +173,8 @@ let check_annot_get_beh f =
   (match f.sspec.spec_terminates with
   | None -> ()
   | Some term_p when term_p.ip_content.pred_content = Ptrue -> ()
-  | _ -> Kernel.fatal "abrupt termination for lemma-functions is not allowed");
+  | Some _ when f.sspec.spec_lemma -> Kernel.fatal "abrupt termination for lemma-functions is not allowed"
+  | Some _ -> ());
   if (List.length f.sspec.spec_behavior = 1) then (
     let beh = (List.hd f.sspec.spec_behavior) in
     if f.sspec.spec_lemma then (
